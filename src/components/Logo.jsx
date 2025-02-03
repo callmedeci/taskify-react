7bd93c23fa451d9dff44c6e58734db1b3c74e491
callmedeci <callmedecim@gmail.com>
2025-02-03 09:28:40 -0800

    Add react fav icon to project

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,15 @@
-import { RectangleStackIcon } from "@heroicons/react/24/solid";
-import { Link } from "react-router";
+import { RectangleStackIcon } from '@heroicons/react/24/solid';
+import { Link } from 'react-router';
 
 function Logo() {
   return (
     <Link
-      to="/"
-      className="flex items-center gap-2 text-zinc-300 transition-colors duration-300 hover:text-zinc-50"
+      to='/'
+      className='flex items-center gap-2 text-zinc-300 transition-colors duration-300 hover:text-zinc-50'
     >
-      <h2 className="text-2xl font-medium">Task Manager</h2>
+      <h2 className='text-2xl font-medium'>Task Manager</h2>
 
-      <RectangleStackIcon className="size-8" />
+      <RectangleStackIcon className='size-8' />
     </Link>
   );
 }
