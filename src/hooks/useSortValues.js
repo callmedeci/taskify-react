3e50fabff4ffcf3d3349c7f6b78fe10c5b83187c
callmedeci <callmedecim@gmail.com>
2025-02-05 11:22:15 -0800

    Fix a small bug

--- src/hooks/useSortValues.js
+++ src/hooks/useSortValues.js
@@ -1,14 +1,14 @@
 import { useUrl } from './useUrl';
 
-export function useSortValues(values, defaultSortBy = 'date-desc') {
+export function useSortValues(values, defaultSortBy = 'created_at-desc') {
   const { readUrl } = useUrl('sortBy');
   const sortBy = readUrl || defaultSortBy;
   const [filed, direction] = sortBy.split('-');
   const modifier = direction === 'asc' ? 1 : -1;
 
-  const sortedValues = values.sort(
-    (a, b) => a[filed].localeCompare(b[filed]) * modifier,
-  );
+  const sortedValues = values
+    .slice()
+    .sort((a, b) => a[filed].localeCompare(b[filed]) * modifier);
 
   return [sortedValues];
 }
